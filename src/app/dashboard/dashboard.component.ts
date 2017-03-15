c94ec00fe4127d957ee2d0959b6872dd7b3adcd5
vandemorc <m.strasinskis@gmail.com>
2017-03-15 20:58:50 +0100

    Refactor arguments

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -22,10 +22,9 @@ export class DashboardComponent implements OnInit {
         this.tasksService.getTasks(),
         this.searchTerms
       )
-      .do((acc: [Task[], string]) => console.log(acc))
-      .map((acc: [Task[], string]) => {
-        const tasks = acc[0] || [];
-        const regExp = new RegExp(acc[1] || '', 'i');
+      .do(([tasks, term]) => console.log(tasks, term))
+      .map(([tasks=[], term]: [Task[], string]) => {
+        const regExp = new RegExp(term || '', 'i');
         return tasks.filter(task => regExp.test(task.name))
       });
   }
