26e50278292596474387bc3516dd3ae8a9497786
vandemorc <m.strasinskis@gmail.com>
2017-03-15 12:57:56 +0100

    Add tasks filter (local)

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {TasksService} from "../tasks.service";
-import {Observable} from "rxjs";
+import {Observable, BehaviorSubject} from "rxjs";
 import {Task} from "../task";
+
+import 'rxjs/add/operator/combineLatest';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,11 +13,25 @@ import {Task} from "../task";
 })
 export class DashboardComponent implements OnInit {
   private tasks: Observable<Task[]>;
+  private searchTerms = new BehaviorSubject<string>('');
 
   constructor(private tasksService: TasksService) { }
 
   ngOnInit(): void {
-    this.tasks = this.tasksService.getTasks();
+    this.tasks = Observable.combineLatest(
+        this.tasksService.getTasks(),
+        this.searchTerms
+      )
+      .do((acc: [Task[], string]) => console.log(acc))
+      .map((acc: [Task[], string]) => {
+        const tasks = acc[0] || [];
+        const regExp = new RegExp(acc[1] || '', 'i');
+        return tasks.filter(task => regExp.test(task.name))
+      });
+  }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
   }
 
 }
